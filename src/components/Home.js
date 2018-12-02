2a3dfb12f495aaf0d88a8ca1ff8b081241dd9842
jakobfp <s0543983@htw-berlin.de>
2018-12-02 13:36:28 +0100

    error handling, loading sign, clear form, closes #7

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import api from './api-config.js';
 
+var Spinner = require('react-spinkit');
+
 const download_file = (url, name) => {
   const link = document.createElement('a');
   link.href = url;
@@ -21,7 +23,8 @@ class Home extends Component {
       download_url: "",
       design: "",
       uploaded_file_path: "",
-      error: ""
+      error: "",
+      isLoading: false
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -37,6 +40,7 @@ class Home extends Component {
 
   handleFileChange(event){
     this.setState({filename: event.target.value});
+    this.setState({error: ""});
 
     const data = new FormData();
     data.append('file', this.fileInput.current.files[0]);
@@ -52,33 +56,57 @@ class Home extends Component {
         this.setState({uploaded_file_path: response.file_path});
       }
       else {
-        this.setState({uploaded: `${response.error}`});
         this.setState({error: response.error});
       }
     });
   }
 
   handleSubmit(event){
-    const params = `file=${this.state.uploaded_file_path}&design=${this.state.design}`;
-    fetch(api.convert+params, {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(response => {
-      if(response.success === true){
-        this.setState({download_url : `http://localhost:5000/api/download?file=${response.file_path}`});
-        download_file(this.state.download_url, response.file_name);
-      } else {
-        this.setState({error: response.error});
-      }
+    this.setState({error: ""});
+    if(this.state.design === ""){
+      this.setState({error: "no design select, please select one!"});
+
+    }
+    else {
+      this.setState({isLoading: true});
+      const convertParams = `file=${this.state.uploaded_file_path}&design=${this.state.design}`;
+      fetch(api.convert+convertParams, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(response => {
+        if(response.success === true){
+          const downloadParams = `file=${response.file_path}`;
+          this.setState({download_url : api.download+downloadParams});
+          download_file(this.state.download_url, response.file_name);
+          this.setState({isLoading: false});
+        } else {
+          this.setState({error: response.error});
+        }
+      });
+      this.clearForm();
+    }
+  }
+
+  clearForm(){
+    this.setState({
+      filename: "",
+      design: "",
+      uploaded: <FontAwesomeIcon icon="times" />,
+      download_url: "",
+      uploaded_file_path: "",
+      error: ""
     });
+    this.fileInput = React.createRef();
   }
 
   render() {
     return (
       <div>
+        {this.state.error != "" ? (<div id="error"><p>{this.state.error}</p></div>) : (<p></p>)}
         <h2>HELLO</h2>
         <p>With this Application you can convert Latex articles and presentations as well as Word documents in the corporate design of HTW Berlin.</p>
+        {this.state.isLoading ? (<Spinner id="loading-spinner" name="wave" fadeIn="none"/>) : (<p></p>)}
         <form onSubmit={this.handleSubmit}>
           <h5>1. Upload file</h5>
           <input type="file" ref={this.fileInput} value={this.state.filename} onChange={this.handleFileChange}/> {this.state.uploaded}
--- src/components/api-config.js
+++ src/components/api-config.js
@@ -2,7 +2,8 @@ const domain = "http://localhost:5000/api";
 
 const api = {
   upload: domain + "/upload",
-  convert: domain + "/convert?"
+  convert: domain + "/convert?",
+  download: domain + "/download?"
 };
 
 export default api;
