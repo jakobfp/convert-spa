2f0559ff22e6518e82522bd4ce687fc89eb44efa
jakobfp <s0543983@htw-berlin.de>
2019-02-17 16:28:41 +0100

    use only /convert request

--- src/components/api-config.js
+++ src/components/api-config.js
@@ -2,9 +2,9 @@ const domain = "http://localhost:5000/api";
 
 const api = {
   upload: domain + "/upload",
-  tex: domain + "/convert_tex?",
-  docx: domain + "/convert_docx_or_odt?",
-  odt: domain + "/convert_docx_or_odt?",
+  tex: domain + "/convert?",
+  docx: domain + "/convert?",
+  odt: domain + "/convert?",
   download: domain + "/download?"
 };
 
